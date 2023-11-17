12c4a2f6e5d2074362d477fe4246d698e850b114
Anthony Gantt <anthony@pcsatease.com>
2023-11-17 10:16:31 -0600

    method: created and tested square. It wworks

--- src/Assessment.java
+++ src/Assessment.java
@@ -1,3 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Assessment {
 
+
+    //1. Write a public static method named square that accepts an integer as input and returns that number times itself.
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static void testSquare() {
+        int result = square(5);
+        System.out.println("Test Square: Square of 5 is " + result);
+    }
 }
+
