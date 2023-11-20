4659e7152ccf5b08b0034f62f556f7edd7367db8
Anthony Gantt <anthony@pcsatease.com>
2023-11-19 18:53:16 -0600

    .

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,22 @@
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.print("Hello ");
+        System.out.print("World!");
+
+        int myFavoriteNumber = 19;
+        System.out.println();
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hi";
+        System.out.println(myString);
+
+//        myString = 'c'; did not compile code java: incompatible types: char cannot be converted to java.lang.String
+
+//        myString = 3.14159; did not compile java: incompatible types: double cannot be converted to java.lang.String
+
+//        long myNumber;
+//        System.out.println(myNumber); did not compile java: variable myNumber might not have been initialized
     }
 }
